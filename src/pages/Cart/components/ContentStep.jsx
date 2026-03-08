19cddd1ef88b41fcca82880ce567b1a15f728a18
conghung205 <nongconghungcba@gmail.com>
2026-03-08 21:10:59 +0700

    fix đường dẫn

--- src/pages/Cart/components/ContentStep.jsx
+++ src/pages/Cart/components/ContentStep.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { SteperContext } from "@/contexts/SteperProvider";
 import ShoppingCart from "@pages/Cart/components/ShoppingCart/ShoppingCart";
 import Checkout from "@pages/Cart/components/Checkout/Checkout";
-import QrPayment from "@pages/Cart/components/QrPayment/Index";
+import QrPayment from "@pages/Cart/components/QrPayment/index";
 
 const ContentStep = () => {
     const { currentStep } = useContext(SteperContext);
